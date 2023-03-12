1fa14b335882edf889d1ef98d808ac318fc1b058
onnoshakil <shakil2.onnorokom@gmail.com>
2023-03-12 12:10:03 +0600

    initial git

---
+++ src/main/kotlin/com/example/springTest/SpringTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springTest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringTestApplication>(*args)
+}
